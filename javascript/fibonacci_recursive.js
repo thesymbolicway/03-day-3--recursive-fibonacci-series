c48cb2c76903c7d8862a209e38dfebd770fb62d3
Daniel <daniel.arisa@yahoo.com>
2023-01-26 23:57:07 -0600

    first commit

---
+++ javascript/fibonacci_recursive.js
@@ -0,0 +1,30 @@
+function fibonacci(n) {
+  // base case
+  if (n < 2) {
+    return n;
+  }
+  // recursive case
+  return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+
+if (require.main === module) {
+  // add your own tests in here
+  console.log("Expecting: 0");
+  console.log("=>", fibonacci(0));
+
+  console.log("");
+
+  console.log("Expecting: 1");
+  console.log("=>", fibonacci(2));
+
+  console.log("");
+
+  console.log("Expecting: 55");
+  console.log("=>", fibonacci(10));
+}
+
+module.exports = fibonacci;
+
+// Please add your pseudocode to this file
+// And a written explanation of your solution
